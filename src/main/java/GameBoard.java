af3232c80f84b454887fcc713d340a2a11f6563a
unknown <x_system16@hotmail.com>
2020-11-28 00:05:39 +0100

    Rettet metoden

--- src/main/java/GameBoard.java
+++ src/main/java/GameBoard.java
@@ -20,39 +20,32 @@ public class GameBoard {
         GUI_Field[] fieldsDesign = new GUI_Field[20];
 
         fieldsDesign[0] = new GUI_Start("Start", "", "Hver gang du passerer start, modtager du 2 kr.", Color.GREEN.darker(), Color.BLACK);
-        fieldsDesign[1] = new GUI_Street("Burgerbaren", "", "Få en burger på parkens bedste burgerbar. ('Bedste' nomineret af egen resturant.)", "1", Color.ORANGE, Color.BLACK);
-        fieldsDesign[2] = new GUI_Street("Pizzariaet", "", "Hvis munden nu ikke er stor nok til den burger.", "1", Color.ORANGE, Color.BLACK);
-        fieldsDesign[3] = new GUI_Chance("?", "", "Træk et chancekort og følg dets anvisninger.", Color.WHITE, Color.BLACK);
-        fieldsDesign[4] = new GUI_Street("Slikbutikken", "", "Til den lille slikmund.", "1", Color.BLUE.brighter(), Color.BLACK);
-        fieldsDesign[5] = new GUI_Street("Iskiosken", "", "Hjælper når solen er fremme.", "1", Color.BLUE.brighter(), Color.BLACK);
-        fieldsDesign[6] = new GUI_Jail();
-        fieldsDesign[6].setSubText("");
-        fieldsDesign[7] = new GUI_Street("Museet", "", "Både sjovt og lærerigt.", "2", Color.PINK, Color.BLACK);
-        fieldsDesign[8] = new GUI_Street("Biblioteket", "", "Lærerigt, men ikke så sjovt som museet.", "2", Color.PINK, Color.BLACK);
-        fieldsDesign[9] = new GUI_Chance("?", "", "Træk et chancekort og følg dets anvisninger.", Color.WHITE, Color.BLACK);
-        fieldsDesign[10] = new GUI_Street("Skaterparken", "", "Husk nu hjelmen.", "2", Color.YELLOW.brighter(), Color.BLACK);
-        fieldsDesign[11] = new GUI_Street("Svømmingpoolen", "", "Ikke løbe ved poolen!.", "2", Color.YELLOW.brighter(), Color.BLACK);
-        fieldsDesign[12] = new GUI_Refuge();
-        fieldsDesign[12].setTitle("Gratis parkering");
-        fieldsDesign[12].setSubText("");
-        fieldsDesign[13] = new GUI_Street("Spillehallen", "", "Forældrenes undskyldning for deres ludomani.", "3", Color.RED, Color.BLACK);
-        fieldsDesign[14] = new GUI_Street("Biografen", "", "Brug tid sammen med familien, uden at snakke med dem.", "3", Color.RED, Color.BLACK);
-        fieldsDesign[15] = new GUI_Chance("?", "", "Træk et chancekort og følg dets anvisninger.", Color.WHITE, Color.BLACK);
-        fieldsDesign[16] = new GUI_Street("Legetøjsbutikken", "", "Hvem siger man ikke kan købe kærlighed.", "3", Color.YELLOW, Color.BLACK);
-        fieldsDesign[17] = new GUI_Street("Dyrehandlen", "", "Hvis familien nu ikke er stor nok.", "3", Color.YELLOW, Color.BLACK);
-        fieldsDesign[18] = new GUI_Shipping();
-        fieldsDesign[18].setTitle("Gå i fængsel");
-        fieldsDesign[18].setSubText("");
-        fieldsDesign[19] = new GUI_Street("Bowlinghallen", "", "Når far skal vise sig.", "4", Color.GREEN, Color.BLACK);
-        fieldsDesign[20] = new GUI_Street("Zoo", "", "Det eneste sted dine forældre tillader dyr.", "4", Color.GREEN, Color.BLACK);
-        fieldsDesign[21] = new GUI_Chance("?", "", "Træk et chancekort og følg dets anvisninger.", Color.WHITE, Color.BLACK);
-        fieldsDesign[22] = new GUI_Street("Vandlandet", "", "Samme som poolen, bare dyrer.", "5", Color.BLUE.brighter(), Color.WHITE);
-        fieldsDesign[23] = new GUI_Street("Strandpromenaden", "", "Sand. Alle. Steder.", "5", Color.BLUE.brighter(), Color.WHITE);
-
+        fieldsDesign[1] = new GUI_Street("Burgerbaren", "1M", "Få en burger på parkens bedste burgerbar. ('Bedste' nomineret af egen resturant.)", "1", Color.ORANGE, Color.BLACK);
+        fieldsDesign[2] = new GUI_Street("Pizzariaet", "1M", "Hvis munden nu ikke er stor nok til den burger.", "1", Color.ORANGE, Color.BLACK);
+        fieldsDesign[3] = new GUI_Street("Slikbutikken", "1M", "Til den lille slikmund.", "1", Color.BLUE.brighter(), Color.BLACK);
+        fieldsDesign[4] = new GUI_Street("Iskiosken", "1M", "Hjælper når solen er fremme.", "1", Color.BLUE.brighter(), Color.BLACK);
+        fieldsDesign[5] = new GUI_Jail();
+        fieldsDesign[5].setSubText("");
+        fieldsDesign[6] = new GUI_Street("Museet", "2M", "Både sjovt og lærerigt.", "2", Color.PINK, Color.BLACK);
+        fieldsDesign[7] = new GUI_Street("Biblioteket", "2M", "Lærerigt, men ikke så sjovt som museet.", "2", Color.PINK, Color.BLACK);
+        fieldsDesign[8] = new GUI_Street("Skaterparken", "3M", "Husk nu hjelmen.", "2", Color.YELLOW.brighter(), Color.BLACK);
+        fieldsDesign[9] = new GUI_Street("Svømmingpoolen", "3M", "Ikke løbe ved poolen!.", "2", Color.YELLOW.brighter(), Color.BLACK);
+        fieldsDesign[10] = new GUI_Refuge();
+        fieldsDesign[10].setTitle("Gratis parkering");
+        fieldsDesign[10].setSubText("");
+        fieldsDesign[11] = new GUI_Street("Spillehallen", "4M", "Forældrenes undskyldning for deres ludomani.", "3", Color.RED, Color.BLACK);
+        fieldsDesign[12] = new GUI_Street("Biografen", "4M", "Brug tid sammen med familien, uden at snakke med dem.", "3", Color.RED, Color.BLACK);
+        fieldsDesign[13] = new GUI_Street("Legetøjsbutikken", "4M", "Hvem siger man ikke kan købe kærlighed.", "3", Color.YELLOW, Color.BLACK);
+        fieldsDesign[14] = new GUI_Street("Dyrehandlen", "4M", "Hvis familien nu ikke er stor nok.", "3", Color.YELLOW, Color.BLACK);
+        fieldsDesign[15] = new GUI_Shipping();
+        fieldsDesign[15].setTitle("Gå i fængsel");
+        fieldsDesign[15].setSubText("");
+        fieldsDesign[16] = new GUI_Street("Bowlinghallen", "5M", "Når far skal vise sig.", "4", Color.GREEN, Color.BLACK);
+        fieldsDesign[17] = new GUI_Street("Zoo", "5M", "Det eneste sted dine forældre tillader dyr.", "4", Color.GREEN, Color.BLACK);
+        fieldsDesign[18] = new GUI_Street("Vandlandet", "5M", "Samme som poolen, bare dyrer.", "5", Color.BLUE.darker(), Color.WHITE);
+        fieldsDesign[19] = new GUI_Street("Strandpromenaden", "5M", "Sand. Alle. Steder.", "5", Color.BLUE.darker(), Color.WHITE);
 
         return fieldsDesign;
     }
-
-
 
 }
