505891180c01f66d23074c647372c78b88b4c3f7
thomas <thomas.arildtoft@live.dk>
2020-11-28 00:28:40 +0100

    Har tilføjet 3 klasser.

---
+++ src/main/java/Dice.java
@@ -0,0 +1,10 @@
+public class Dice {
+
+    public int diceValue() {
+        int min = 1;
+        int max = 6;
+        int range = max - min + 1;
+        return (int)((Math.random() * range) + min);
+    }
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        GameBoard gameBoard = new GameBoard();
+
+    }
+}
